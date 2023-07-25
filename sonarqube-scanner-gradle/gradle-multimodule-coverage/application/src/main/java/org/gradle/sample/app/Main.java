0a092d079985888cc529fed00a5b3b529af36fb2
Austin Smith <austin.smith@sonarsource.com>
2023-07-25 12:12:48 -0500

    cosmetic change

--- sonarqube-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
+++ sonarqube-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
@@ -9,7 +9,7 @@ import static org.gradle.sample.app.MessageUtils.getMessage;
 public class Main {
     public static void main(String[] args) {
         LinkedList tokens;
-            tokens = split(getMessage());
+                tokens = split(getMessage());
         System.out.println(join(tokens));
     }
 }
