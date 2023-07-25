3a75f6950d4fc513e550680a0b726cbd39854ba5
Austin Smith <austin.smith@sonarsource.com>
2023-07-25 12:16:37 -0500

    add some code

--- sonarqube-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
+++ sonarqube-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
@@ -8,7 +8,12 @@ import static org.gradle.sample.app.MessageUtils.getMessage;
 
 public class Main {
     public static void main(String[] args) {
+        int test;
+        test = 0;
+        test += 1;
+        System.out.println(test);
         LinkedList tokens;
+        
                 tokens = split(getMessage());
         System.out.println(join(tokens));
     }
